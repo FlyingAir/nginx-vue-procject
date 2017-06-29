9882a59e9ea859b75326b9fa3854f35eec19a6fa
FlyingAir <827199132@qq.com>
2017-06-30 01:22:17 +0800

    banner 、searchbar

--- nginx-vue/src/App.vue
+++ nginx-vue/src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
     <mt-tabbar v-model="selected">
-      <mt-tab-item  id="tab1" >
+      <mt-tab-item  id="tab1">
         <i slot="icon" class="main-icon" @click="toMain"></i>
       </mt-tab-item>
       <mt-tab-item id="tab2" >
-        <i slot="icon" class="music-icon" @click="toMusic"></i> 
+        <i slot="icon" class="music-icon" @click="toMusic"></i>
       </mt-tab-item>
       <div class="playing-model" id="playing-model">
         <i class="playing-model-icon">
           <img class="loading-model" src="../static/loading.gif" height="25" width="32"></img>
-        </i> 
+        </i>
       </div>
       <mt-tab-item id="tab3" >
        <i slot="icon" class="center-icon" @click="toCenter"></i>
@@ -29,23 +29,23 @@
 import router from "./router"
 export default {
   data: function() {
-    return { 
+    return {
       visible: false,
       selected: 1,
        }
   },
   methods:{
     toMain(){
-      router.push({path:"/main",name:"main"});
+      router.push({name:"main"});
     },
     toMusic(){
-      router.push({path:"/music",name:"music"});
+      router.push({name:"music"});
     },
     toSearch(){
-      router.push({path:"/search",name:"search"});
+      router.push({name:"search"});
     },
     toCenter(){
-      router.push({path:"/center",name:"center"});
+      router.push({name:"center"});
     }
   },
 }
--- nginx-vue/src/pages/Main.vue
+++ nginx-vue/src/pages/Main.vue
@@ -1,9 +1,19 @@
 <template>
- 
-    <mt-swipe :auto="4000">
-       <mt-swipe-item v-for="bannerItem in bannerList.banners"><img class="banner-img" v-bind:src="bannerItem.pic" alt="" width="100%"/></mt-swipe-item>
-  		
-	</mt-swipe>
+  <div class="main-page">
+    <div class="search-bar">
+      <div class="search-content">
+        <i class="search-icon"></i>
+        <input type="text" name="" value="" placeholder="Jay" readonly>
+      </div>
+    </div>
+
+
+    <mt-swipe :auto="4000" >
+      <mt-swipe-item v-for="(bannerItem,index) in bannerList.banners">
+        <img class="banner-img" v-bind:src="bannerItem.pic" alt='' width="100%" :index="index" ref="pic"/>
+      </mt-swipe-item>
+    </mt-swipe>
+  </div>
 </template>
 
 <script>
@@ -11,37 +21,85 @@ import router from "../router"
 export default {
   data: function() {
     return {
-      visible: false,
-      bannerList:''
+      bannerList: ''
     }
   },
-  created: function () {
+  created: function() {
     this.getBanner();
   },
-  methods:{
-  	getBanner:function(){
-  		this.$http.get("/api/banner").then(response => {
-    		this.bannerList=response.data;
-    		console.log(this.bannerList);
-		}, response => {
-    		console.log("error");
-		})
-  	},
-  	bannerHeight:function(){
+  mounted:function(){
 
-  	}
+ },
+  methods: {
+    getBanner: function() {
+      this.$http.get("/api/banner").then(response => {
+        this.bannerList = response.data;
+        console.log(this.bannerList);
+      }, response => {
+        console.log("error");
+      })
+    },
   }
 }
 </script>
 <style lang="scss">
-.mint-tab-container{
-	height:100%;
+.main-page{
+  width: 100%;
+  .search-bar{
+    height:30px;
+    background-color: #ffdf7d;
+    padding-top: 6px;
+    .search-content{
+      width: 80%;
+      margin:0px auto;
+      height:24px;
+      border-radius: 20px 20px;
+      overflow: hidden;
+      position: relative;
+      .search-icon{
+        width:16px;
+        height:16px;
+        top:5px;
+        left:7px;
+        background-color: red;
+        position: absolute;
+        background: url("/static/sp.png")  no-repeat;
+        background-size:400px 300px;
+        background-position:(-356*0.5)+px (-172*0.5)+px;
+      }
+      input{
+        color:#eee;
+        font-size:16px;
+        height:24px;
+        padding-left:40px;
+        width: 100%;
+        outline: none;
+        border:none;
+      }
+    }
+  }
 }
-.mint-tab-container-wrap{
-	height:100%;
+.mint-tab-container {
+    overflow: visible;
+    width: 100vw;
+    height: (100*420/1080)+vw;
 }
-	.mint-swipe{
-		width:100%;
-		height:100%;
-	}
+.mint-tab-container-wrap {
+    width: 100%;
+    height: 100%;
+}
+.mint-swipe {
+    width: 100%;
+    height: 100%;
+}
+.banner-img{
+  display: block;
+}
+.mint-swipe-items-wrap > div{
+  width: 100%;
+  height: 100%;
+}
+.mint-swipe-items-wrap{
+  width: 100%;
+}
 </style>
